c6f4ca3b16e404338233f88d0c775c57a683dba4
Thiago Rabelo <thiagorabelo1@gmail.com>
2025-09-11 13:55:35 -0300

    Download from initializr

---
+++ exp-01/src/main/java/udemy/spring/exp_01/Startup.java
@@ -0,0 +1,13 @@
+package udemy.spring.exp_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp01Application.class, args);
+	}
+
+}
---
+++ exp-01/src/test/java/udemy/spring/exp_01/StartupTests.java
@@ -0,0 +1,13 @@
+package udemy.spring.exp_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
