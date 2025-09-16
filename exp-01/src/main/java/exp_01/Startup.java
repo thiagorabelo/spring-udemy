c7759a2588993e43e541e5ba6c98384d3ae66625
Thiago Rabelo <thiagorabelo1@gmail.com>
2025-09-15 23:03:20 -0300

    Primeiro Endpoint

--- exp-01/src/main/java/exp_01/Startup.java
+++ exp-01/src/main/java/exp_01/Startup.java
@@ -1,13 +1,13 @@
-package udemy.spring.exp_01;
+package exp_01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Exp01Application {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Exp01Application.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
---
+++ exp-01/src/main/java/exp_01/model/Greeting.java
@@ -0,0 +1,4 @@
+package exp_01.model;
+
+public record Greeting(long id, String content) {
+}
